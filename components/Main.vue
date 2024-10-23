c90c87328c3319036c7da97adcd90b4e13d73187
Your Name <you@example.com>
2024-10-23 21:36:07 +0500

    feat: added header and card for videos(atube-1)

---
+++ components/Main.vue
@@ -0,0 +1,59 @@
+<template>
+  <main>
+    <a class="card" href="#">
+      <div class="card__image"></div>
+      <div class="card__description">
+        <p>Yes</p>
+      </div>
+    </a>
+    <a class="card" href="#">
+      <div class="card__image"></div>
+      <div class="card__description">
+        <p>Brawl Stars</p>
+      </div>
+    </a>
+    <a class="card" href="#">
+      <div class="card__image"></div>
+      <div class="card__description">
+        <p>WOT</p>
+      </div>
+    </a>
+  </main>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+main {
+  width: 75vw;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(18em, 20em));
+  /* justify-content: space-between; */
+  gap: 1rem;
+}
+
+.card {
+  cursor: pointer;
+  /* width: clamp(8em, 15em, 1fr); */
+  height: 14em;
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  color: black;
+}
+
+.card__image {
+  background-color: lightgrey;
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+}
+
+.card__description {
+  padding: 6px 4px 0 4px;
+  font-size: 1.3rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
-  
+  <div>
+    <Header></Header>
+    <Main></Main>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
 })
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+* {
+  padding: 0;
+  margin: 0;
+  font-family: "Monsterrat", sans-serif;
+}
+</style>
